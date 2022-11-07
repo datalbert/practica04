65e5eeaa6fd287d96de2a38302c93814c101d92e
Alberto Avila Fernandez <albertoavilafernandez@MacBook-Pro-de-Alberto.local>
2022-11-07 23:14:42 +0100

    primer archivo

---
+++ Practica04.cc
@@ -0,0 +1,19 @@
+
+#include "ns3/command-line.h"
+#include "ns3/core-module.h"
+#include "ns3/simulator.h"
+#include "Fuente.h"
+#include "Transmisor.h"
+#include <queue>
+#include <stack>
+
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("Ejercicio02"); 
+
+
+int main (int argc, char *argv[])
+{
+    
+}
