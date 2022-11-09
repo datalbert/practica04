982e931ecd9869ae5b818524d22c466c06caa435
datalbert <albertoavfdez2000@gmail.com>
2022-11-09 14:11:26 +0100

    apartado 2 completo

--- Practica04.cc
+++ Practica04.cc
@@ -9,56 +9,113 @@
 #include "ns3/ipv4-address-helper.h"
 #include "ns3/ipv4-interface-container.h"
 #include "ns3/udp-server.h"
-
-
-#include "ns3/point-to-point-channel.h"
+#include "ns3/udp-client-server-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/data-rate.h"
+#include "ns3/channel.h"
+#include "ns3/nstime.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("Practica04"); 
+NS_LOG_COMPONENT_DEFINE("Practica04");
 
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    Ptr<Node> n_servidor=CreateObject<Node>();
-    Nodecontainer c_todos (n_servidor);
-    Ptr<Node> n_cliente=CreateObject<Node>();
-    c_todos.add(n_cliente);
-    
+    //Configuración de la simulación
+    //-----------------------------------------------------------
+    // Resolución del simulador:
+    Time::SetResolution(Time::NS);
+    //----------------------------------------------------
+    // Parametros y valores por defecto:
+    Time duracion_simulacion = Time("10ms");
+    uint32_t max_paq_cliente=0;
+    Time intervalo=Time("50ns")
+    uint32_t tam_paq=100;
+    UintegerValue max_paq_cliente;
+    TimeValue delay_channel=Time("");
+    //----------------------------------------------------
+    CommandLine cmd();
+
+    cmd.AddValue("duracion_simulacion", "Duracion de la simulacion", duracion_simulacion);
+    cmd.AddValue("maximoPaquetes del cliente udp", max_paq_clientes);
+
+
+
+    cmd.Parse(argc, argv);
+
+    //Fin configuración.
+    //------------------------------------------------------------
+
+    // Creamos nodos y los añadimos a un contenedor
+    Ptr<Node> n_servidor = CreateObject<Node>();
+    NodeContainer c_todos(n_servidor);
+    Ptr<Node> n_cliente = CreateObject<Node>();
+    c_todos.Add(n_cliente);
 
     InternetStackHelper h_pila;
-    h_pila.SetIpv6StackInstall (false);
-    h_pila.Install (c_todos);
-     
-    //Capa ppp, capa común, lo hacemos directamente desde el contenedor. 
+    h_pila.SetIpv6StackInstall(false);
+    h_pila.Install(c_todos);
+
+    // Capa ppp --> Instalamos ppp a todos los nodos
     PointToPointHelper h_pro;
-    NetDeviceContainer c_dispositivo=h_pro.install(c_todos);
+    NetDeviceContainer c_dispositivos = h_pro.Install(c_todos);
 
-    //Capa ip
-    Ipv4AddressHelper direcciones("10.1.13.0","255.255.255.0");
-    Ipv4InterfacesContainer c_interfaces=direcciones.Assign(c_todos);
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+    // Capa ip -->
+    Ipv4AddressHelper h_direcciones("10.1.13.0", "255.255.255.0");
+    Ipv4InterfaceContainer c_interfaces = h_direcciones.Assign(c_dispositivos);
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
+    // Capa de aplicación.
 
-    //Capa de aplicación.
-    UdpEchoServerHelper h_echoServer(20);
-    ApplicationContainer c_serverApps = h_echoServer.Install (n_servidor);
-    
-    UdpEchoClientHelper h_echoClient(c_interfaces.GetAddress(1),21);
-    ApplicationContainer c_clientApps = h_echoClient.Install (n_cliente);	
-    
-    //Añadimos el remoto al udp client
-    c_clientApps.Get(0)->SetRemote(
-    	c_interfaces.GetAddress(0),
-    	c_serverApps.Get(0)->
+    //-->UDP
+    UdpServerHelper h_Server(20);
+    ApplicationContainer c_serverApps = h_Server.Install(n_servidor);
+
+    UdpClientHelper h_Client(c_interfaces.GetAddress(1), 21);
+    ApplicationContainer c_clientApps = h_Client.Install(n_cliente);
+
+    // Añadimos el remoto al udp client
+    UintegerValue port_servidor;
+    c_serverApps.Get(0)->GetObject<UdpServer>()->GetAttribute("Port",port_servidor);
+
+    c_clientApps.Get(0)->GetObject<UdpClient>()->SetRemote(
+        c_interfaces.GetAddress(0),
+        port_servidor.Get()
     );
-    	
-    c_clientApps.SetRemote
+
+    DataRateValue v_serv;
+    c_dispositivos.Get(0)->GetAttribute("DataRate",v_serv);
+    DataRateValue v_client;
+    c_dispositivos.Get(1)->GetAttribute("DataRate",v_client);
+
     
-    //le decimos al cliente cual es el servidor.
+    c_dispositivos.Get(0)->GetChannel()->GetAttribute("Delay",delay_channel);
+
+    UintegerValue max_paq_cliente;
+    c_clientApps.Get(0)->GetObject<UdpClient>()->GetAttribute("MaxPackets",max_paq_cliente);
     
+    TimeValue intervalo;
+    c_clientApps.Get(0)->GetObject<UdpClient>()->GetAttribute("Interval",intervalo);
     
+    //Sumario de nodos y ispositivos.
+    //UintegerValue id_nodoServidor;
+    //c_todos.Get(0)->GetAttribute("Id",id_nodoServidor);
+    NS_LOG_INFO("ID nodo servidor:" << c_todos.Get(0)->GetId());
+    NS_LOG_INFO("ID nodo cliente:" << c_todos.Get(1)->GetId());
+    NS_LOG_INFO("Dispositivo de red Servidor:\n"
+        << "Dirección Mac: " << c_dispositivos.Get(0)->GetAddress() <<", Velocidad de Tx: "<<v_serv.Get().GetBitRate());
+    NS_LOG_INFO("Dispositivo de red Cliente:\n"
+        << "Dirección Mac: " << c_dispositivos.Get(1)->GetAddress() <<", Velocidad de Tx: "<<v_client.Get().GetBitRate());
+    NS_LOG_INFO("Retardo del canal "<< delay_channel.Get());
+    NS_LOG_INFO("Puerto de escucha del servidor "<< port_servidor.Get());
+    NS_LOG_INFO("Nº maximo de paquetes que el cliente puede enviar: "<< max_paq_cliente.Get());
+    NS_LOG_INFO("Intervalo de envio: "<< intervalo.Get());
+
     
-    
-    
+    //---------------------------------------------------------------
+    //Lanzamos simulación
+    NS_LOG_INFO("Arranca simulación");
+    Simulator::Stop(duracion_simulacion);
+    Simulator::Run();
+    NS_LOG_INFO("Fin simulación");
 }
